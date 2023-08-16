7c1162162f4fdac27aceb716ee97ffbe75582bc8
Alan <lnjnr9215@gmail.com>
2023-08-16 17:08:59 -0300

    resgatando dados do banco de dados

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
 
+import { MomentService } from 'src/app/services/moment.service';
+
+import { Moment } from 'src/app/Moments';
+
+import { environment } from 'environments/environments';
+
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  allMoments: Moment[] = []
+  moments: Moment[] = []
+  baseApiUrl = environment.baseApiUrl
 
+  constructor(
+    private momentService: MomentService
+  ) {}
+
+  ngOnInit(): void {
+    this.momentService.getMoments().subscribe((items) => {
+      const data = items.data
+      data.map((item) => {
+        item.created_at = new Date(item.created_at!).toLocaleDateString('pt-br')
+      })
+
+      this.allMoments = data
+      this.moments = data
+    })
+  }
 }
--- src/app/services/moment.service.ts
+++ src/app/services/moment.service.ts
@@ -3,6 +3,8 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 import { Moment } from '../Moments';
+import { Response } from '../Response';
+
 
 import { environment } from 'environments/environments';
 
@@ -14,6 +16,10 @@ export class MomentService {
   private apiUrl = `${this.baseApiUrl}api/moments`
 
   constructor(private http: HttpClient) { }
+
+  getMoments(): Observable<Response<Moment[]>> {
+    return this.http.get<Response<Moment[]>>(this.apiUrl)
+  }
 
   createMoment(formData : FormData): Observable<FormData> {
     return this.http.post<FormData>(this.apiUrl, formData)
