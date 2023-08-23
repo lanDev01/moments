12683d525c372f29fcbf65c8ec1813f2a40ffd0c
Alan <lnjnr9215@gmail.com>
2023-08-23 15:34:15 -0300

    aplicando funcionabilidade de editar

--- src/app/components/pages/edit-moment/edit-moment.component.ts
+++ src/app/components/pages/edit-moment/edit-moment.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Moment } from 'src/app/Moments';
 import { MomentService } from 'src/app/services/moment.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { MessagesService } from 'src/app/services/messages.service';
 
 @Component({
   selector: 'app-edit-moment',
@@ -14,7 +15,9 @@ export class EditMomentComponent implements OnInit {
 
   constructor(
     private momentService: MomentService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private messagesService: MessagesService,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
@@ -23,5 +26,24 @@ export class EditMomentComponent implements OnInit {
     this.momentService.getMoment(id).subscribe((item) => {
       this.moment = item.data
     })
+  }
+
+  async editHandler(momentData: Moment) {
+    const id = this.moment.id
+
+    const formData = new FormData()
+
+    formData.append('title', momentData.title)
+    formData.append('description', momentData.description)
+
+    if(momentData.image) {
+      formData.append('image', momentData.image)
+    }
+
+    await this.momentService.updateMoment(id!, formData).subscribe()
+
+    this.messagesService.add(`Momento ${id} foi atualizado com sucesso!`)
+
+    this.router.navigate(['/'])
   }
 }
